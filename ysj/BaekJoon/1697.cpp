02dd55cd86e30314d63aa1b2d62e3e84f3d421e7
ysj-arad <seon1474@gmail.com>
2020-03-16 11:00:16 +0900

    6번 틀린이유: visit 배열 사이즈 [100000] -> [100000+1]

--- ysj/BaekJoon/1697.cpp
+++ ysj/BaekJoon/1697.cpp
@@ -3,45 +3,56 @@
 #define MAX 100000
 using namespace std;
 
-queue<int> line;
+queue<pair<int,int>> line;
 
 int N,K;
-void BFS(int pos);
-int step=0;
+void BFS(int src);
+int visit[MAX]={0,};
 
 int main(void)
 {
 	cin>>N>>K;
 
 	BFS(N);
-	cout<<"\n\n";
-	cout<<step<<"\n";
+	cout<<line.front().second<<"\n";
 	return 0;
 }
 
 void BFS(int src)
 {
 	int dst;
+	int depth=0;
 	int back,front,zump;
-	line.push(src);
-
-	cout<<"push first src: "<<src<<"\n";
-	
+	line.push({src,depth});
+	visit[src]=true;
+		
 	while(!line.empty())
 	{
-	  dst=line.front();
-	  cout<<dst<<" ";
-	  line.pop();
+	  dst=line.front().first;
+	  depth=line.front().second;
 
 	  if(dst == K)
 		  break;
+	  line.pop();
 
 	  back=dst-1;
 	  front=dst+1;
 	  zump=dst*2;
 
-
-
+	  if(back>=0 && !visit[back])
+	  {
+			  line.push({back,depth+1});
+			  visit[back]=true; 
+	  }
+	  if(front<=MAX && !visit[front] )
+	  {
+			  line.push({front,depth+1});
+			  visit[front]=true;
+	  }
+	  if(zump<=MAX && !visit[zump])
+	  {
+			  line.push({zump,depth+1});
+			  visit[zump]=true;
+	  }
 	}
-
 }
