0f03e722c0274193246129f4f6dc8eb051d8cecd
ysj-arad <seon1474@gmail.com>
2020-04-08 17:10:22 +0900

    baekjoon 14500 problem solving(ing)

--- ysj/BaekJoon/14500.cpp
+++ ysj/BaekJoon/14500.cpp
@@ -2,47 +2,142 @@
 
 using namespace std;
 
+int N,M;
 int map[500][500];
+int visit[500][500]={0,};
+int Dmax=0;
+int Tmax=0;
+int dy[4]={-1,1,0,0};
+int dx[4]={0,0,-1,1};
 
-int block[2][4][4]=
-{
-	{
-	 {1,1,1,1},
-	 {0,0,0,0},
-	 {0,0,0,0},
-	 {0,0,0,0}
-	},
-
-	{
-	 1,1,1,0,
-	 0,0,1,0,
-	 0,0,0,0,
-	 0,0,0,0
-	}
-
-};
-
-
+void DFS(int y, int x,int step, int sum);
+void Tsum(int y,int x);
 int main(void)
 {
-	int N,M;
-/*
+	
 	cin>>N>>M;
 
-	for(int i=0;i<N;i++)
+	for(int i=0; i<N;i++)
 		for(int j=0;j<M;j++)
 			cin>>map[i][j];
-*/
-	for(int i=0; i<2;i++)
-		for(int j=0;j<4;j++){
-			for(int k=0;k<4;k++)
-				printf("%d ",block[i][j][k]);
-			cout<<endl;
+
+	for(int i=0; i<N;i++)
+	{
+		for(int j=0;j<M;j++)
+		{
+		  visit[i][j]=true;
+		  DFS(i,j,1,map[i][j]);
+		  visit[i][j]=false;
+		  Tsum(i,j);
+		}	
+	}
+
+	if(Dmax>Tmax)
+		cout<<Dmax<<"\n";
+	else
+		cout<<Tmax<<"\n";
+
+	return 0;
+}
+void DFS(int y,int x, int step, int sum)
+{
+	int ty,tx;
+	
+	if(step == 4)
+	{
+		if(sum>Dmax)
+			Dmax=sum;
+		return;
+	}
+
+	for(int i=0; i<4; i++)
+	{
+		ty=y+dy[i];
+		tx=x+dx[i];
+		if(ty == N || ty == -1 || tx == M || tx == -1)
+			continue;
+		if(visit[ty][tx] == 0)
+		{
+			visit[ty][tx]=true;
+			DFS(ty,tx,step+1,sum+map[ty][tx]);
+			visit[ty][tx]=false;
 		}
-
-
-
-
+	}
 
 
 }
+void Tsum(int y, int x)
+{
+	int sum;
+
+	int hx=x+1;
+	int Hx=x+2;
+	int lx=x-1;
+    int Lx=x-2;
+
+	int hy=y+1;
+	int Hy=y+2;
+	int ly=y-1;
+	int Ly=y-2;
+
+    /*  ***
+	     *  */
+	if( My<N && Hx < M)
+	{
+		sum=map[y][x]+map[y][hx]+map[y][Hx]+map[hy][hx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+	/* *
+	   **
+	   * */
+
+	if(Hy<N && hx<M)
+	{
+		sum=map[y][x]+map[hy][x]+map[hy][hx]+map[Hy][x];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+	/*  
+	*/
+        	   
+	if( Hx<M && ly >=0)
+	{
+		sum=map[y][x]+map[y][hx]+map[y][Hx]+map[ly][lx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+	if(Ly >=0 && hx < M)
+	{
+		sum=map[y][x]+map[ly][x]+map[Ly][x]+map[ly][hx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+	if(Ly >=0 && lx >=0)
+	{
+		sum=map[y][x]+map[ly][x]+map[Hy][x]+map[ly][lx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+
+ 	if(hy<N && Lx>=0)
+	{
+		sum=map[y][x]+map[y][lx]+map[y][Lx]+map[hy][lx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+	if(ly>=0 && Hx<M)
+	{
+		sum=map[y][x]+map[y][hx]+map[y][Hx]+map[ly][hx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+	if(Hy<N && lx>=0)
+	{
+		sum=map[y][x]+map[hy][x]+map[Hy][x]+map[hy][lx];
+		if(sum>Tmax)
+			Tmax=sum;
+	}
+
+}
+
