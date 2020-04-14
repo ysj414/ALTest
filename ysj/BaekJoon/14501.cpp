07f918b67c95029f0dd38c2ed4fc729ec1a787cf
ysj-arad <seon1474@gmail.com>
2020-04-14 14:26:18 +0900

    baekjoon 14501 problem solve new type problem (Dynamic Programming) I use recursive function

--- ysj/BaekJoon/14501.cpp
+++ ysj/BaekJoon/14501.cpp
@@ -9,16 +9,18 @@ int P[15];
 int N;
 int max_profit=0;
 
-int DP(int index, int capacity)
+int DP(int index)
 {
-	if(index >= N-1)
-	        return 0;
-	
-	if(T[index]<=capacity)
-		return max((DP(index+T[index],capacity-T[index])+P[index]),DP(index+1,capacity));
+	if(index == N)
+		return 0;
+
+	if(T[index]<=N-index)
+	{
+		return max(DP(index+T[index])+P[index],DP(index+1));
+	}
 	else
-		return DP(index+1,capacity);
-}	
+		return DP(index+1);
+}
 
 int main(void)
 {
@@ -26,8 +28,9 @@ int main(void)
 
 	for(int i=0; i<N;i++)
 		cin>>T[i]>>P[i];
-	
-	max_profit=DP(0,N);
+
+    max_profit=DP(0);
+
 	
 	cout<<max_profit<<"\n";
 		
