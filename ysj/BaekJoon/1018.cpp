41d29502fcd1e2b7300bd7d42b8a9a94d8eccb1e
ysj-laptop <seon1474@gmail.com>
2020-07-20 07:31:49 -0700

    baekjoon problem 1018 solve
    Use Static declation 2-dimension array
    
    Mistake
    1. 2-dimension array value input wrong
    (correct)
    WB BW WB
    BW WB BW
    (my wrong input)
    WB WB WB
    BW BW BW
    2. indexing miss
    8x8 input
    (correct)
    for i=0; i<=0;i++
    (my wrong input)
    for i=0; i<0;i++
    LOOP rotate at least one time.
    3. variable initialization position error
    (correct)
    inner for loop
    (my wrong answer)
    outside for loop

--- ysj/BaekJoon/1018.cpp
+++ ysj/BaekJoon/1018.cpp
@@ -4,49 +4,67 @@ using namespace std;
 
 char map[52][52];
 
-char Wfirst[9][9]={"WBWBWBWB","WBWBWBWB","WBWBWBWB","WBWBWBWB","WBWBWBWB","WBWBWBWB","WBWBWBWB","WBWBWBWB"};
-char Bfirst[9][9]={"BWBWBWBW","BWBWBWBW","BWBWBWBW","BWBWBWBW","BWBWBWBW","BWBWBWBW","BWBWBWBW","BWBWBWBW"};
+char Wfirst[9][9]={"WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW"};
+char Bfirst[9][9]={"BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB"};
 int N,M;
 int main(void)
 {
 	cin>>N>>M;
-	for(int i=0;i<N;i++)
+	for(int i=0; i<N; i++)
 	{
-		for(int j=0; j<M;j++)
-			cin>>map[i][j];
+		for(int j=0; j<M; j++)
+		   cin>>map[i][j];
 	}
+/*
+	cout<<"debug"<<"\n";
+	for(int i=0;i<N;i++){
+	    for(int j=0;j<M;j++)
+		cout<<map[i][j];
+	    cout<<"\n";
+	}
+	cout<<"debug"<<"\n";
+	for(int i=0;i<N;i++){
+	    for(int j=0;j<M;j++)
+		cout<<Wfirst[i][j];
+	    cout<<"\n";
+	}
+	cout<<"debug"<<"\n";
+	for(int i=0;i<N;i++){
+	    for(int j=0;j<M;j++)
+		cout<<Bfirst[i][j];
+	    cout<<"\n";
+	}
+*/
+
 
 	int MIN=999999;
 	
 	int NewN=N-PIXEL;
 	int NewM=M-PIXEL;
-
-	for(int i=0; i<NewN; i++)
+//	cout<<NewN<<" "<<NewM<<"\n";
+	for(int i=0; i<=NewN; i++)
 	{
-		int TEMP_WMIN=0,TEMP_BMIN=0;
-		for(int j=0; j<NewM; j++)
+		for(int j=0; j<=NewM; j++)
 		{
+		int TEMP_WMIN=0,TEMP_BMIN=0;
 			for(int k=i; k<i+PIXEL; k++)
 			{
 				for(int m=j; m<j+PIXEL; m++)
 				{
+//				    cout<<"M(i,j) W(i,j) B(i,j)"<<"("<<k<<","<<m<<" "<<map[k][m]<<" "<<Wfirst[k-i][m-j]<<" "<<Bfirst[k-i][m-j]<<"\n";
 					if(map[k][m] != Wfirst[k-i][m-j])
 						TEMP_WMIN++;
 					if(map[k][m] != Bfirst[k-i][m-j])
 						TEMP_BMIN++;
 				}
-				
-				if(TEMP_WMIN < TEMP_BMIN)
-				{
-					if(TEMP_WMIN > MIN)
-						MIN=TEMP_WMIN;
-				}
-				else
-				{
-					if(TEMP_BMIN > MIN)
-						MIN=TEMP_BMIN;
-				}
 			}
+					if(TEMP_WMIN < MIN)
+						MIN=TEMP_WMIN;
+					if(TEMP_BMIN < MIN)
+						MIN=TEMP_BMIN;
+			
+//			cout<<"WMIN: "<<TEMP_WMIN<<"BMIN: "<<TEMP_BMIN<<"\n";
+//			cout<<"**"<<"\n";
 		}
 	}
 
