0005ca1fe2614c89abe03676a7e5694ed77f761a
ysj-arad <seon1474@gmail.com>
2021-04-28 11:10:14 +0900

    baekjoon problem 11559 solving I can't find count example.

--- ysj/BaekJoon/11559.cpp
+++ ysj/BaekJoon/11559.cpp
@@ -4,11 +4,6 @@
 #include<unistd.h>
 using namespace std;
 
-#define RED "R"
-#define GRN "G"
-#define BLU "B"
-#define PUP "P"
-#define YEL "Y"
 #define DOT '.'
 
 
@@ -45,7 +40,7 @@ int main(void)
  	  fgets(buf,8,stdin);
 	  strncpy(field[i],buf,7);
 	}
-	print_field();
+//	print_field();
 
 	while(1)
 	{
@@ -55,19 +50,15 @@ int main(void)
 		 int ret = is_chain(11,i);
 		 if(ret)
 		 {
-//			 remove_puyo(11,i);
-			 printf("remove puyo\n");
-			 print_field();
-
-			 for(int t=0; t<6; t++)
-			 	move_field(11,t);
-			 printf("change puyo\n");
-			 print_field();
 			 if(chain < 1)
 				 chain++;
-
+//			 print_field();
 		 }
 		}
+		if(chain)
+			for(int t=0; t<6; t++)
+				move_field(11,t);
+//		print_field();	
 		result+=chain;
 		if(chain == 0)
 			break;		
@@ -84,11 +75,14 @@ int is_chain(int x, int y)
 
 	if(field[x][y] == DOT)
 		return false;
+
+
 	q.push({x,y});
 
 	for(int i=0; i<12; i++)
 		for(int j=0; j<6; j++)
 			visit[i][j]=0;
+	visit[x][y]=1;
 
 	while(!q.empty())
 	{
@@ -101,16 +95,16 @@ int is_chain(int x, int y)
 			int ny = ty+dy[i];
 			if(nx > 12 || nx < 0 || ny > 6 || ny < 0)
 				continue;
-			if( field[nx][ny] == field[x][y] && visit[nx][ny] == 0)
+			if( (field[nx][ny] == field[x][y]) && visit[nx][ny] == 0)
 			{
-				printf("%d %d\n",nx,ny);
 				q.push({nx,ny});
 				visit[nx][ny]=1;
 				check_chain++;
 			}
 		}
 	}
-
+	
+//	printf("check_chain : %d", check_chain);
 	if(check_chain>=4)
 	{
 		for(int i=0; i<12; i++)
@@ -124,34 +118,6 @@ int is_chain(int x, int y)
 
 }
 
-/*void remove_puyo(int x, int y)
-{
-	char puyo = field[x][y];
-	queue<pair<int,int>> q;
-
-	q.push({x,y});
-	field[x][y]= DOT;
-	while(!q.empty())
-	{
-		int tx = q.front().first;
-		int ty = q.front().second;
-		q.pop();
-
-		for(int i=0; i<4; i++)
-		{
-		  int nx = tx+dx[i];
-		  int ny = ty+dy[i];
-		  if(nx > 12 || nx < 0 || ny > 6 || ny < 0)
-			  continue;
-		  if(field[nx][ny] == puyo)
-		  {
-			  q.push({nx,ny});
-			  field[nx][ny]=DOT;
-		  }
-		}
-	}
-}*/
-
 void move_field(int x, int y)
 {
 		int count=0;
@@ -160,10 +126,15 @@ void move_field(int x, int y)
 
 		for(int j=11; j>=0; j--)
 		{
+			count++;
+//			printf("field:%c\n",field[j][y]);
 			if(field[j][y] != DOT)
-				count++;
+			{
+				count--;
+				break;
+			}
 		}
-		printf("count:%d",count);
+//		printf("count:%d\n",count);
 		if(count == 12)
 			return;
 
